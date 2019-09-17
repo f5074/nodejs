c460220efdfa4fd8b88ce2c8ee763f34693fad03
f5074 <oseongryu@gmail.com>
2019-09-18 01:26:34 +0900

    change router to  Router Controler

--- api/user/index.js
+++ api/user/index.js
@@ -1,68 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const controller = require('./user.controller');
 
-// pesudo code
-let users = [
-    {
-      id: 1,
-      name: 'alice'
-    },
-    {
-      id: 2,
-      name: 'bek'
-    },
-    {
-      id: 3,
-      name: 'chris'
-    }
-  ]
+router.get('/users', controller.index);
 
-router.get('/', (req, res) => {
-  return res.json(users);
-});
+router.get('/users/:id', controller.show);
 
-router.get('/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-  if (!id) {
-    return res.status(400).json({error: 'Incorrect id'});
-  }
+router.delete('/users/:id', controller.destroy);
 
-  let user = users.filter(user => user.id === id)[0]
-  if (!user) {
-    return res.status(404).json({error: 'Unknown user'});
-  }
+router.post('/users', controller.create);
 
-  return res.json(user);
-});
 
-router.delete('/:id', (req, res) => {
-const id = parseInt(req.params.id, 10);
-  if (!id) {
-    return res.status(400).json({error: 'Incorrect id'});
-  }
 
-  const userIdx = users.findIndex(user => user.id === id);
-  if (userIdx === -1) {
-    return res.status(404).json({error: 'Unknown user'});
-  }
-
-  users.splice(userIdx, 1);
-  res.status(204).send();
-});
-
-router.post('/', (req, res) => {
-    const name = req.body.name || '';
-  if (!name.length) {
-    return res.status(400).json({error: 'Incorrenct name'});
-  }
-  const id = users.reduce((maxId, user) => {
-    return user.id > maxId ? user.id : maxId
-  }, 0) + 1;
-  const newUser = {
-    id: id,
-    name: name
-  };
-  users.push(newUser);
-  return res.status(201).json(newUser);
-});
 module.exports = router;
