495d572fee04b84a205c3bece0f9eb6cb15323ce
f5074 <oseongryu@gmail.com>
2019-10-28 15:09:04 +0900

    In deploy, using npm start and using 80 port

--- a_education/b_express/app.js
+++ a_education/b_express/app.js
@@ -4,14 +4,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var port=process.env.PORT || 80;
 
 app.get('/', (req, res) => {
   res.send('Hello World!\n');
  });
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(port);
 
 // pesudo code
 let users = [
